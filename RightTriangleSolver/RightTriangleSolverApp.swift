ad250bdb2bf7c3ff20d4238bbde3cf2a26d6637d
pAkalpa <pasinduakalpa1998@gmail.com>
2023-03-19 09:50:42 +0530

    Initial Commit

---
+++ RightTriangleSolver/RightTriangleSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightTriangleSolverApp.swift
+//  RightTriangleSolver
+//
+//  Created by Buddhika Kalupahana on 2023-03-19.
+//
+
+import SwiftUI
+
+@main
+struct RightTriangleSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
